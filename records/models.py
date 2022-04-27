4f05fd6a3590d44857e07347e9eb35f77b15717d
ajmdotdev <contact.ajmendoza@gmail.com>
2022-04-27 14:45:20 +0800

    feat: add Request model
    TODO: add enum field for 'status' column

--- records/models.py
+++ records/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from library.models import Book
+from django.contrib.auth.models import User
 # Create your models here.
+class Request(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, null=True, blank=True, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    # add enum field for status
+
+# def get_expiry():
+#     return datetime.today() + timedelta(days=15)
