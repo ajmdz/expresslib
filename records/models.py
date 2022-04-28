f63f3adef221e62ff1bdce4c96a7798a896555df
ajmdotdev <contact.ajmendoza@gmail.com>
2022-04-28 13:09:09 +0800

    Add status field to Request model

--- records/models.py
+++ records/models.py
@@ -3,10 +3,16 @@ from library.models import Book
 from django.contrib.auth.models import User
 # Create your models here.
 class Request(models.Model):
+
+    class RequestStatus(models.TextChoices):
+        APPROVED = 'approved'
+        DECLINED = 'declined'
+        PENDING = 'pending'
+
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, null=True, blank=True, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
-    # add enum field for status
+    status = models.CharField(max_length=8, choices=RequestStatus.choices, default=RequestStatus.PENDING)
 
 # def get_expiry():
 #     return datetime.today() + timedelta(days=15)
