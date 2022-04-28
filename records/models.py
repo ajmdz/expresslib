27f3fd102cd5245eec2de773fdab47c4481df979
ajmdotdev <contact.ajmendoza@gmail.com>
2022-04-28 14:33:40 +0800

    fix: Replace status field (enum) in Record model to returned (boolean)

--- records/models.py
+++ records/models.py
@@ -18,10 +18,6 @@ class Request(models.Model):
         return str(self.book) + " - " + str(self.user) 
 
 class Record(models.Model):
-    class BorrowStatus(models.TextChoices):
-        ONGOING = 'ONGOING'
-        RETURNED = 'RETURNED'
-        OVERDUE = 'OVERDUE'
 
     def get_returnDate():
         return datetime.today() + timedelta(days=7)
@@ -29,7 +25,7 @@ class Record(models.Model):
     request = models.ForeignKey(Request, null=True, blank=True, on_delete=models.CASCADE)
     issue_date = models.DateTimeField(auto_now_add=True)
     return_date = models.DateTimeField(default=get_returnDate)
-    status = models.CharField(max_length=8, choices=BorrowStatus.choices, default=BorrowStatus.ONGOING)
+    returned = models.BooleanField(default=False)
     # fine = models.DecimalField(default=0, decimal_places=2, max_digits=7)
     
     def __str__(self):
