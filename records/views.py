e718bbb88be690a396f49d2997f03a7f15eef993
ajmdotdev <contact.ajmendoza@gmail.com>
2022-04-30 14:26:16 +0800

    feat: add request confirmation view in records app

--- records/views.py
+++ records/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from library.models import Book
+from .models import Request
+from django.contrib.auth.models import User
 # Create your views here.
+def confirmRequest(request, pk):
+    user_id = User.objects.get(id=request.user.id)
+    bookObj = Book.objects.get(id=pk)
+    context = {'book':bookObj}
+    
+    newRequest = Request(user=user_id, book=bookObj)
+
+    if request.method == 'POST':
+        newRequest.save()
+
+    return render(request, 'records/confirmRequest.html', context)
