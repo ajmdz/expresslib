0b2d7a6ac2cc51354596945cb85102869bc801c7
ajmdotdev <contact.ajmendoza@gmail.com>
2022-05-01 01:12:58 +0800

    feat: add urls and views for admin Request tasks

--- records/urls.py
+++ records/urls.py
@@ -4,6 +4,9 @@ app_name = "records"
 
 urlpatterns = [
     path('confirm/<str:pk>/', views.confirmRequest, name="confirm"),
+    path('requests/', views.fetchRequests, name="borrow-requests"),
+    path('approve/<int:pk>/', views.approveRequest, name="approve-request"),
+    path('decline/<int:pk>/', views.declineRequest, name="decline-request"),
 ]
 
 
--- records/views.py
+++ records/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from library.models import Book
-from .models import Request
+from .models import Request, Record
 from django.contrib.auth.models import User
 # Create your views here.
 def confirmRequest(request, pk):
@@ -12,5 +12,40 @@ def confirmRequest(request, pk):
 
     if request.method == 'POST':
         newRequest.save()
+        return redirect('library:book-detail', pk) # back to detail view
 
     return render(request, 'records/confirmRequest.html', context)
+
+# ADMIN-ONLY VIEWS
+def fetchRequests(request):
+    requests = Request.objects.filter(status='PENDING').order_by('date_created')
+    # for item in requests:
+    #     foo = item.user.username +" "+ item.book.title
+    #     print(foo)
+    context = {'requests':requests}
+    return render(request, 'records/admin-requests.html', context)
+
+def approveRequest(request, pk):
+    item = Request.objects.get(id=pk)
+    item.status = "APPROVED"
+    book = Book.objects.get(id=item.book.id)
+    book.available = False
+    
+    if request.method == 'POST':
+        item.save()
+        record = Record(request=item)
+        record.save()
+        book.save()
+        return redirect('records:borrow-requests')
+
+    return render(request, 'records/confirm-approve.html')
+
+def declineRequest(request, pk):
+    item = Request.objects.get(id=pk)
+    item.status = "DECLINED"
+    
+    if request.method == 'POST':
+        item.save()
+        return redirect('records:borrow-requests')
+
+    return render(request, 'records/confirm-decline.html')
