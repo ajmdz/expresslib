7ac9a6d519d81e10a5b9c73da3c1abb059cc235c
ajmdotdev <contact.ajmendoza@gmail.com>
2022-05-02 09:30:50 +0800

    feat: add route for fetching Records

--- records/urls.py
+++ records/urls.py
@@ -5,6 +5,7 @@ app_name = "records"
 urlpatterns = [
     path('confirm/<str:pk>/', views.confirmRequest, name="confirm"),
     path('requests/', views.fetchRequests, name="borrow-requests"),
+    path('records/', views.fetchRecords, name="records"),
     path('approve/<int:pk>/', views.approveRequest, name="approve-request"),
     path('decline/<int:pk>/', views.declineRequest, name="decline-request"),
 ]
