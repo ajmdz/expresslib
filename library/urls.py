81dcc023086b6b3d290fd1b2734849da66b5f824
ajmdotdev <contact.ajmendoza@gmail.com>
2022-05-04 23:44:44 +0800

    feat: add routes for addAuthor and addPublisher views

--- library/urls.py
+++ library/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
     path('book/<str:pk>/', views.bookDetail, name="book-detail"),
 
     path('manage/', views.manageBooks, name="manage-books"),
-    path('add/', views.addBook, name="add-book"),
+    path('add/book/', views.addBook, name="add-book"),
+    path('add/author/', views.addAuthor, name="add-author"),
+    path('add/publisher/', views.addPublisher, name="add-publisher"),
     path('edit/<str:pk>/', views.editBookDetail, name="edit-book-detail"),
     path('delete/<str:pk>/', views.deleteBook, name="delete-book"),
 
