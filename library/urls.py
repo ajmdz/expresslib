de3b4367eed00470a372948c5d4e3538e7ac5d4f
ajmdotdev <contact.ajmendoza@gmail.com>
2022-05-15 12:59:54 +0800

    feat: update library urls

--- library/urls.py
+++ library/urls.py
@@ -9,12 +9,13 @@ urlpatterns = [
     path('', views.userDashboard, name="books"),
     path('book/<str:pk>/', views.bookDetail, name="book-detail"),
 
-    path('manage/', views.manageBooks, name="manage-books"),
-    path('add/book/', views.addBook, name="add-book"),
-    path('add/author/', views.addAuthor, name="add-author"),
-    path('add/publisher/', views.addPublisher, name="add-publisher"),
-    path('edit/<str:pk>/', views.editBookDetail, name="edit-book-detail"),
-    path('delete/<str:pk>/', views.deleteBook, name="delete-book"),
+    path('libadmin/book/<str:pk>/', views.adminBookDetail, name="admin-book-detail"),
+    path('libadmin/manage/', views.manageBooks, name="manage-books"),
+    path('libadmin/add/book/', views.addBook, name="add-book"),
+    path('libadmin/add/author/', views.addAuthor, name="add-author"),
+    path('libadmin/add/publisher/', views.addPublisher, name="add-publisher"),
+    path('libadmin/edit/<str:pk>/', views.editBookDetail, name="edit-book-detail"),
+    path('libadmin/delete/<str:pk>/', views.deleteBook, name="delete-book"),
 
 
 
